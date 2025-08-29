f4e20dee9cb3251e1b4c408cd12ff82374be6d0c
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2025-08-29 14:53:51 +0200

    test

--- api/index.js
+++ api/index.js
@@ -29,10 +29,7 @@ async function connectToDatabase() {
 
   if (!cached.promise) {
     cached.promise = mongoose
-      .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+      .connect(process.env.MONGO_URI)
       .then((mongoose) => mongoose);
   }
 

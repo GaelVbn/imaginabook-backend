d76576820e49c974f5ffb4876ac2cd7cae1a852f
Gaël Vanbeveren <gael.vanbeveren98@gmail.com>
2025-08-29 15:20:17 +0200

    scripts

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
